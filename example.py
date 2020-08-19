2c4829ebbe38c92752b90d4e30b8725260ccfffd
Nivesh Birangal <niveshbirangal@gmail.com>
2020-08-19 16:28:06 +0530

    Example.py refers adobe illustrator

--- example.py
+++ example.py
@@ -1,24 +1,25 @@
 import rpc
 import time
+from time import mktime
 
 print("Demo for python-discord-rpc")
-client_id = '467570845993271307' #Your application's client ID as a string. (This isn't a real client ID)
-rpc_obj = rpc.DiscordIpcClient.for_platform(client_id) #Send the client ID to the rpc module
+client_id = '467570845993271307'  # Your application's client ID as a string. (This isn't a real client ID)
+rpc_obj = rpc.DiscordIpcClient.for_platform(client_id)  # Send the client ID to the rpc module
 print("RPC connection successful.")
 
 time.sleep(5)
-start_time = time.time()
+start_time = mktime(time.localtime())
 while True:
     activity = {
-            "state": "Active on: iMac Pro",
-            "details": "Designing Logo",
+            "state": "Adobe",
+            "details": "Editing",
             "timestamps": {
                 "start": start_time
             },
             "assets": {
-                "small_text": "adobe",
+                "small_text": "Adobe",
                 "small_image": "adobe",
-                "large_text": "illustrator",
+                "large_text": "Illustrator",
                 "large_image": "illustrator"
             }
         }
