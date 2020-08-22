a63ec7737c481f34fdbf2c2e86ae651e5a866893
Nivesh Birangal <niveshbirangal@gmail.com>
2020-08-22 15:47:12 -0500

    Update example.py

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ import rpc
 import time
 from time import mktime
 
-print("Demo for python-discord-rpc")
+print("Demo for python-discord-rpc Adobe illustrator")
 client_id = '467570845993271307'  # Your application's client ID as a string. (This isn't a real client ID)
 rpc_obj = rpc.DiscordIpcClient.for_platform(client_id)  # Send the client ID to the rpc module
 print("RPC connection successful.")
